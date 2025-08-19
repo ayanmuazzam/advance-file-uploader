551d285cea337117282d07dcf5c158953a306047
Ayan Muazzam <ayan.muazzam@unitedsol.net>
2025-08-19 15:20:41 +0500

    Clean up: Remove advanced-file-uploader folder after successful merge

--- advanced-file-uploader/front/templates/woocommerce_template_loop_add_to_cart.php
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript">
-	jQuery(document).ready(function () {
-		jQuery("a[data-product_id=<?php echo filter_var($extendons_upload_files_needle_ids); ?>]").remove();
-	});
-</script>
